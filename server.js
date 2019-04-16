fa494a03d3d366436c731c4a5b9dc98229301c72
Juan C. Gonzalez <juan.gonzalez@onsolve.com>
2019-04-16 14:13:46 -0400

    enable https

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-var app = require("./app");
-var PORT = process.env.PORT || 3000;
+var http = require("http");
+var https = require("https");
+var fs = require("fs");
+var path = require("path");
 
-app.listen(PORT, function() {
-    console.log("Server running on PORT %s", PORT);
+var app = require("./app");
+
+var key = fs.readFileSync(path.resolve("certs/selfsigned.key"), "utf-8");
+var cert = fs.readFileSync(path.resolve("certs/selfsigned.crt"), "utf-8");
+var credentials = { key, cert };
+
+var HTTP_PORT = process.env.HTTP_PORT || 3000;
+var HTTPS_PORT = process.env.HTTPS_PORT || 5000;
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(HTTP_PORT, function() {
+    console.log("HTTP Server running on PORT %s", HTTP_PORT);
+})
+
+httpsServer.listen(HTTPS_PORT, function() {
+    console.log("HTTPS Server running on PORT %s", HTTPS_PORT);
 })
